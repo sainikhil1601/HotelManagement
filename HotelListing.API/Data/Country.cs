1115d889e1ee3f43ada60e0a1cd3ebf94c59dd37
Daniel Borges Gamboa <dborges30324@ufide.ac.cr>
2023-10-31 20:04:22 -0600

    feat: created DB and migrations

---
+++ HotelListing.API/Data/Country.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelListing.API.Data
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Shortname { get; set; }
+        public virtual IList<Hotel> Hotels { get; set; }
+        
+    }
+}
