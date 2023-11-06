f551f736d2e4107086bb32a8888c30f2218fe95e
Daniel <dborges30324@ufide.ac.cr>
2023-11-06 15:27:59 -0600

    feat: created model for country and installed automapper

---
+++ HotelListing.API/Models/Country/CreateCountryModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.API.Models.Country
+{
+    public class CreateCountryModel
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Shortname { get; set; }
+
+        
+    }
+}
