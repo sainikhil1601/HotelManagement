b0508f6dff25cd583d3413126cc13f45ceb105af
Daniel Borges Gamboa <dborges30324@ufide.ac.cr>
2023-11-15 19:18:42 -0600

    feat: setting up registration and login endpoint

---
+++ HotelListing.API/Contracts/IAuthManager.cs
@@ -0,0 +1,12 @@
+﻿using HotelListing.API.Models.Users;
+using Microsoft.AspNetCore.Identity;
+
+namespace HotelListing.API.Contracts
+{
+    public interface IAuthManager
+    {
+        Task<IEnumerable<IdentityError>> Register(ApiUserModel usermodel);
+        Task<bool> Login(ApiLoginModel apiLoginModel);
+
+    }
+}
---
+++ HotelListing.API/Repository/AuthManager.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using HotelListing.API.Contracts;
+using HotelListing.API.Data;
+using HotelListing.API.Models.Users;
+using Microsoft.AspNetCore.Identity;
+
+namespace HotelListing.API.Repository
+{
+    public class AuthManager: IAuthManager
+    {
+        private readonly IMapper _mapper;
+        private readonly UserManager<ApiUser> _userManager;
+
+        public AuthManager(IMapper mapper, UserManager<ApiUser> userManager)
+        {
+            this._mapper = mapper;
+            this._userManager = userManager;
+        }
+
+        public async Task<bool> Login(ApiLoginModel apiLoginModel)
+        {
+            bool isValidUser = false;
+            try
+            { 
+           var user =  await _userManager.FindByEmailAsync(apiLoginModel.Email);
+                if (user is null)
+                {
+                    return default;
+                }
+                isValidUser = await _userManager.CheckPasswordAsync(user, apiLoginModel.Password);
+
+                if (!isValidUser)
+                {
+                    return default;
+                }
+
+            }
+            catch (Exception) 
+            {
+                throw;
+            }
+            return isValidUser;
+
+        }
+
+        public async Task <IEnumerable<IdentityError>> Register(ApiUserModel usermodel) { 
+        
+            var user = _mapper.Map<ApiUser>(usermodel);
+            user.UserName = usermodel.Email;
+
+            var result = await _userManager.CreateAsync(user, usermodel.Password);
+
+            if (result.Succeeded) 
+            {
+
+                await _userManager.AddToRoleAsync(user, "User");
+            
+            }
+
+            return result.Errors;
+        
+        }
+    }
+}
